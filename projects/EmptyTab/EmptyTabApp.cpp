7962c9266bd6e66bdbd1eb9c1e855fe6ff4de854
Pushkar Kolhe <pushkar@cc.gatech.edu>
2011-09-30 01:42:37 -0400

    Corrected a bug in EmptyTab projects

--- projects/EmptyTab/EmptyTabApp.cpp
+++ projects/EmptyTab/EmptyTabApp.cpp
@@ -1,14 +1,18 @@
 #include "RSTApp.h"
 #include "EmptyTab.h"
-#include <Tabs/PlanningTab.h>
+/* To include Planning Tab, ensure that Tab/PlanningTab is present in SRC_WX for
+ * librst/CMakeLists.txt. Then uncomment the two lines below */
+
+// #include <Tabs/PlanningTab.h>
 
 extern wxNotebook* tabView;
 
 class EmptyTabApp : public RSTApp {
 	virtual void AddTabs() {
 		tabView->AddPage(new EmptyTab(tabView), wxT("EmptyTab"));
-		tabView->AddPage(new PlanningTab(tabView), wxT("PlanningTab"));
+		// tabView->AddPage(new PlanningTab(tabView), wxT("PlanningTab"));
 	}
 };
 
 IMPLEMENT_APP(EmptyTabApp)
+
