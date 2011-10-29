1e8846ddb02acb98a2087e1490b28068cb7e6ff3
Ana <ahuaman3@gatech.edu>
2011-10-29 15:40:29 -0400

    Included VCollide in GRIP

---
+++ src/VCollide/CMakeFiles/gvcollide.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ana/Research/Grip/grip/src/VCollide/NBody.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/NBody.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/PairData.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/PairData.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/RAPID.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/RAPID.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/VCollide.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/VCollide.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/VInternal.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/VInternal.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/build.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/build.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/collide.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/collide.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/overlap.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/overlap.cpp.o"
+  "/home/ana/Research/Grip/grip/src/VCollide/sample_client.cpp" "/home/ana/Research/Grip/grip/src/VCollide/CMakeFiles/gvcollide.dir/sample_client.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_FILE_OFFSET_BITS=64"
+  "_LARGE_FILES"
+  "__WXGTK__"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ src/VCollide/CMakeFiles/gvcollide.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gvcollide.dir/VCollide.cpp.o"
+  "CMakeFiles/gvcollide.dir/NBody.cpp.o"
+  "CMakeFiles/gvcollide.dir/collide.cpp.o"
+  "CMakeFiles/gvcollide.dir/overlap.cpp.o"
+  "CMakeFiles/gvcollide.dir/RAPID.cpp.o"
+  "CMakeFiles/gvcollide.dir/VInternal.cpp.o"
+  "CMakeFiles/gvcollide.dir/sample_client.cpp.o"
+  "CMakeFiles/gvcollide.dir/build.cpp.o"
+  "CMakeFiles/gvcollide.dir/PairData.cpp.o"
+  "../../lib/libgvcollide.pdb"
+  "../../lib/libgvcollide.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gvcollide.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
