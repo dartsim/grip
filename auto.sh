b7f6290330c00a01874e88f189cdd40502c2bed2
Pushkar Kolhe <pushkarkolhe@gmail.com>
2013-03-06 00:04:58 -0500

    Added auto.sh to create debians

--- auto.sh
+++ auto.sh
@@ -3,56 +3,43 @@
 PRG=$0
 usage()
 {
-    echo "usage: auto [<args>]" 
+    echo "usage: ./auto.sh [<args>]" 
     echo ""
     echo "The most commonly used commands are:"
-    echo "  generate_debs   Generates debian on 32 or 64 bit machines"
-    echo "  generate_docs   Generates website documentation"
-    echo "  upload_web      Uploads the new website"
+    echo "  debs <precise/quantal>  Generates debian on 32 or 64 bit machines"
+    echo "  docs <doxygen>          Generates website documentation"
     echo ""
     exit 1
 }
 
 debs()
 {
-mac=$(echo `uname -m`)
-echo ${mac}
-if [ ${mac} = "x86_64" ]; then
-  echo "Generating 64 bit deb"
-  cmake -D CPACK_SYSTEM_NAME=amd64 .
+echo Building deb for $1
+if [ "$1" = "quantal" ]; then
+  echo "quantal deb uses libboost1.49"
+  sed -i 's/precise/quantal/g' CMakeLists.txt 
+  sed -i 's/1.46/1.49/g' CMakeLists.txt
+  cmake .
   cpack -G DEB
 else
-  echo "Generating 32 bit deb"
-  cmake -D CPACK_SYSTEM_NAME=i386 .
+  echo "precise deb uses libboost1.46"
   cpack -G DEB
 fi
 }
 
-web()
-{
-scp *.deb pushkar7@golems.org:~/dart.golems.org/downloads/
-cd docs
-mv html grip
-tar czf grip.tar.gz grip
-scp grip.tar.gz pushkar7@golems.org:~/dart.golems.org/
-ssh pushkar7@golems.org 'cd dart.golems.org; tar -xvf grip.tar.gz'
-}
-
-[ "$#" -lt 1 ] && usage
+[ "$#" -lt 2 ] && usage
 
 # parse commandline
 while [ $# -gt 0 ]
 do
       arg="$1"
+      sys="$2"
       case "$arg" in
-         generate_debs)
-         debs    
+         debs)
+         debs $sys
          ;;
-         generate_docs)
-         doxygen
-         ;;
-         upload_web)
-         web
+         docs)
+         make docs
          ;;
          --) shift; break;;  # no more options
          -*) usage ;; 
